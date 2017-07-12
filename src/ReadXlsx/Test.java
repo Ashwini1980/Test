ac06c149024be05a55a36ca251cf87e2ad5ae15e
Chhabi <Chhabi@DESKTOP-9ITJO8F>
2017-07-13 00:47:29 +0530

    Completed

---
+++ src/ReadXlsx/Test.java
@@ -0,0 +1,127 @@
+package ReadXlsx;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeMap;
+
+import org.apache.poi.util.SystemOutLogger;
+
+public class Test {
+/*	
+	int Num;
+	String Name;
+	
+	Test (String Name, int Num) {
+		this.Name=Name;
+		this.Num=Num;
+
+	}
+
+	
+	   public static <K, V> void printMap(Map<K, V> map) {
+	        for (Map.Entry<K, V> entry : map.entrySet()) {
+	            System.out.println("Key : " + entry.getKey()
+					+ " Value : " + entry.getValue());
+	        }
+	    }
+	
+
+	 public static void main(String[] args) {
+		 
+
+		 
+		    Test t1 = new Test ("Ashwini", 100);		    
+		    Test t2 = new Test ("Sai", 10);
+		    Test t3 = new Test ("Chhabi", 30);
+			
+		    
+			Map<String, Integer> unsortMap = new HashMap<String, Integer>();
+			unsortMap.put(t1.Name, t1.Num);
+			unsortMap.put(t2.Name, t2.Num);
+			unsortMap.put(t3.Name, t3.Num);
+			
+			System.out.println("Unsort Map......");
+	        printMap(unsortMap);
+	        
+	        System.out.println("\nSorted Map......By Key");
+	        Map<String, Integer> treeMap = new TreeMap<String, Integer>(unsortMap);
+	        printMap(treeMap);
+			//unsortMap.put("Ashwini", 100);
+			//unsortMap.put("Sai", 10);
+			//unsortMap.put("Chhabi", 30);
+			 
+	}*/
+			
+
+/*	 public static void main(String[] args) {
+		 
+		 System.out.println("Enter a num");
+		 Scanner sc = new Scanner (System.in);		 
+		 int input = sc.nextInt();*/
+		 
+/*		 int i,m=0,flag=0;    
+		  //int n=17;//it is the number to be checked  
+		  m=input/2;    
+		  for(i=2;i<=m;i++){    
+		   if(input%i==0){    
+		   System.out.println("Number is not prime");    
+		   flag=1;    
+		   break;    
+		   }    
+		  }    
+		  if(flag==0)    
+		  System.out.println("Number is prime"); 
+		  */
+		  
+		
+/*         //loop through the numbers one by one
+         for(int i=1; i < input; i++){
+                
+                boolean isPrime = true;
+                
+                 //check to see if the number is prime
+                 for(int j=2; j < i ; j++){
+                        
+                         if(i % j == 0){
+                                 isPrime = false;
+                                 break;
+                         }
+                 }
+                 // print the number
+                if(isPrime){
+                	System.out.print(i + " ");
+                }
+                         
+         }
+         */
+/*         
+         String s1 = "Test";
+         String s2 = "ing";
+         
+         @SuppressWarnings("rawtypes")
+		Stack sc1 = new Stack ();
+         sc1.add(s1);
+         sc1.add(s2);         
+         
+         String s3 = sc1.peek().toString() + sc1.peek().toString();
+         System.out.println(s3);
+         System.out.println(sc1);
+         
+         //String s4 = sc1.pop().toString()+ sc1.pop();
+        // System.out.println(s4);
+         //System.out.println(sc1);
+         
+         String s5 = sc1.remove(0).toString()+sc1.peek();
+         System.out.println(s5);
+         System.out.println(sc1);*/
+         
+
+}
